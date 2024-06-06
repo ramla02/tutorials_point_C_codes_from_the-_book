710fbcdbeaad5e277fbab95c21fcf60cf4f6323b
ramla02 <ramla252017@gmail.com>
2024-06-06 07:45:54 +0300

    this is a worm up

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - Entery poin
+* this is a hello world program, the first one in the book.
+* Return: 0 for the successful execution.
+*/
+int main(void)
+{
+	printf("Hello, World\n");
+
+	return (0);
+}
